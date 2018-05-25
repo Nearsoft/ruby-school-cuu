805ab5b04c03e90b21d957230823326817bfcdae
Andrei Helo <andrei.helo@points.com>
2018-05-25 16:29:01 -0600

    Class 5
    Devise installation and configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+
+  root to: "events#index"
+
   resources :events
   get 'events/:id/register', to: 'events#register', as: 'register_to_event'
   post 'events/:id/register', to: 'events#register_user', as: 'register_user'
